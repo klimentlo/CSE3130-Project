06d415b579c0348c420f79b5b48a4e1646eff103
Kliment Lo <Kliment Lo>
2023-11-20 18:26:47 -0700

    go make collisions work

--- brick.py
+++ brick.py
@@ -5,71 +5,77 @@ import pygame
 from color import Color
 
 
+
 class Brick(MySprite):
-    def __init__(self):
+    def __init__(self, WIDTH=100, HEIGHT=50):
         MySprite.__init__(self)
 
-        self.__BRICK_SPRITE = Box(100, 50)
+        self.__BRICK_SPRITE = Box(WIDTH, HEIGHT)
         self._SURFACE = self.__BRICK_SPRITE.getSurface()
 
 
         # Top collision box
-        self.__BRICK_TOP = Box(90, 5)
+        self.__BRICK_TOP = Box(WIDTH*0.9, HEIGHT*0.1)
         self.__BRICK_TOP.setColor(Color.RED)
 
         # Bottom collision box
-        self.__BRICK_BOTTOM = Box(90, 5)
+        self.__BRICK_BOTTOM = Box(WIDTH*0.9, HEIGHT*0.1)
         self.__BRICK_BOTTOM.setColor(Color.RED)
 
         # Left collision box
-        self.__BRICK_LEFT = Box(5, 40)
+        self.__BRICK_LEFT = Box(WIDTH*0.05, HEIGHT*0.8)
         self.__BRICK_LEFT.setColor(Color.BLUE)
 
         # Right collision box
-        self.__BRICK_RIGHT = Box(5, 40)
+        self.__BRICK_RIGHT = Box(WIDTH*0.05, HEIGHT*0.8)
         self.__BRICK_RIGHT.setColor(Color.BLUE)
 
         # CORNERS :(
-        self.__TOP_LEFT_CORNER = Box(5, 5)
+        self.__TOP_LEFT_CORNER = Box(WIDTH*0.05, HEIGHT*0.1)
         self.__TOP_LEFT_CORNER.setColor(Color.GREEN)
 
-        self.__TOP_RIGHT_CORNER = Box(5, 5)
+        self.__TOP_RIGHT_CORNER = Box(WIDTH*0.05, HEIGHT*0.1)
         self.__TOP_RIGHT_CORNER.setColor(Color.GREEN)
 
-        self.__BOTTOM_LEFT_CORNER = Box(5, 5)
+        self.__BOTTOM_LEFT_CORNER = Box(WIDTH*0.05, HEIGHT*0.1)
         self.__BOTTOM_LEFT_CORNER.setColor(Color.GREEN)
 
-        self.__BOTTOM_RIGHT_CORNER = Box(5, 5)
+        self.__BOTTOM_RIGHT_CORNER = Box(WIDTH*0.05, HEIGHT*0.1)
         self.__BOTTOM_RIGHT_CORNER.setColor(Color.GREEN)
 
 
 
-    # SIDES
+    # --- SIDES
+
+    # Top
     def getTopSurface(self):
         return self.__BRICK_TOP.getSurface()
 
     def getTopPOS(self):
         return self.__BRICK_TOP.getPOS()
 
+    # Bottom
     def getBottomSurface(self):
         return self.__BRICK_BOTTOM.getSurface()
 
     def getBottomPOS(self):
         return self.__BRICK_BOTTOM.getPOS()
 
+    # Left
     def getLeftSurface(self):
         return self.__BRICK_LEFT.getSurface()
 
     def getLeftPOS(self):
         return self.__BRICK_LEFT.getPOS()
 
+    # Right
     def getRightSurface(self):
         return self.__BRICK_RIGHT.getSurface()
 
     def getRightPOS(self):
         return self.__BRICK_RIGHT.getPOS()
 
-    # CORNERS
+    # --- CORNERS
 
     # Top Left
     def getTopLeftSurface(self):
@@ -101,7 +107,7 @@ class Brick(MySprite):
 
 
 
-    def setBrickPOS(self, X=100, Y=100):
+    def setBrickPOS(self, X, Y):
         '''
         sets all collision boxes and surfaces to the set location :D
         :param X:
@@ -112,9 +118,9 @@ class Brick(MySprite):
         BRICK.setPOS(X, Y)
         # Sides         If this\/ doesn't work, change the X to Brick.getX() :D
         self.__BRICK_TOP.setPOS(X + BRICK.getWidth()//2 - self.__BRICK_TOP.getWidth()//2, Y)
-        self.__BRICK_BOTTOM.setPOS(X + BRICK.getWidth()//2 - self.__BRICK_TOP.getWidth()//2, Y+self.__BRICK_SPRITE.getHeight()-self.__BRICK_BOTTOM.getHeight())
-        self.__BRICK_LEFT.setPOS(X, Y + BRICK.getHeight()//2-self.__BRICK_BOTTOM.getHeight()//2)
-        self.__BRICK_RIGHT.setPOS(X + self.__BRICK_SPRITE.getWidth()-self.__BRICK_RIGHT.getWidth(), BRICK.getHeight()//2-self.__BRICK_BOTTOM.getHeight()//2)
+        self.__BRICK_BOTTOM.setPOS(X + BRICK.getWidth()//2 - self.__BRICK_TOP.getWidth()//2, Y + self.__BRICK_SPRITE.getHeight()-self.__BRICK_BOTTOM.getHeight())
+        self.__BRICK_LEFT.setPOS(X, Y + BRICK.getHeight()//2-self.__BRICK_LEFT.getHeight()//2)
+        self.__BRICK_RIGHT.setPOS(X + self.__BRICK_SPRITE.getWidth()-self.__BRICK_RIGHT.getWidth(), Y + BRICK.getHeight()//2-self.__BRICK_RIGHT.getHeight()//2)
         
         # Corners
         self.__TOP_LEFT_CORNER.setPOS(X, Y)
@@ -128,7 +134,7 @@ class Brick(MySprite):
       
         # Brick
         WINDOW.getSurface().blit(BRICK.getSurface(), BRICK.getPOS())
-        
+
         # Sides
         WINDOW.getSurface().blit(BRICK.getLeftSurface(), BRICK.getLeftPOS())
         WINDOW.getSurface().blit(BRICK.getRightSurface(), BRICK.getRightPOS())
@@ -136,10 +142,16 @@ class Brick(MySprite):
         WINDOW.getSurface().blit(BRICK.getBottomSurface(), BRICK.getBottomPOS())
         
         # Corners
-        WINDOW.getSurface().blit(BRICK.getTopLeftSurface, BRICK.getTopLeftPOS())
-        WINDOW.getSurface().blit(BRICK.getTopRightSurface, BRICK.getTopRightPOS()())
-        WINDOW.getSurface().blit(BRICK.getBottomLeftSurface, BRICK.getBottomLeftPOS())
-        WINDOW.getSurface().blit(BRICK.getBottomRightSurface, BRICK.getBottomRightPOS())
+        WINDOW.getSurface().blit(BRICK.getTopLeftSurface(), BRICK.getTopLeftPOS())
+        WINDOW.getSurface().blit(BRICK.getTopRightSurface(), BRICK.getTopRightPOS())
+        WINDOW.getSurface().blit(BRICK.getBottomLeftSurface(), BRICK.getBottomLeftPOS())
+        WINDOW.getSurface().blit(BRICK.getBottomRightSurface(), BRICK.getBottomRightPOS())
+"""
+    def isCollision(self, WIDTH, HEIGHT, POS):
+        '''
+        use width, height, and POS of the ball, and see if it collides with the corresoponding brick
+        '''
+        """
 
 
 
@@ -150,16 +162,39 @@ if __name__ == "__main__":
     from window import Window
     pygame.init()
     WINDOW = Window("Asteroid Test", 800, 600, 30)
-    BRICK = Brick()
-    BRICK.setBrickPOS(50,50)
+    BRICKS = []
+    for i in range(25):
+        BRICKS.append(Brick())
+    X = 0
+    Y = 10
+    START_X = 50
+    BRICK_GAP = 50
+    LAYERING = 0
+    for BRICK in BRICKS:
+        if X <= WINDOW.getWidth() - BRICK.getWidth():
+            BRICK.setBrickPOS(X+START_X + LAYERING, Y)
+            X += BRICK.getWidth() + BRICK_GAP
+
+        else:
+            X = 0
+            Y += BRICK.getHeight() + BRICK_GAP
+            LAYERING *= -1
+            BRICK.setBrickPOS(X+START_X + LAYERING, Y)
+            X += BRICK.getWidth() + BRICK_GAP
+
+        BRICK.blitBrick()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
+
         WINDOW.clearScreen()
-        BRICK.blitBrick()
+
+        for BRICK in BRICKS:
+            BRICKS[i].blitBrick()
         WINDOW.updateFrame()
 
         
